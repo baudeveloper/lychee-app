916d0a6477778e5725f4c11c0447693648514c27
baudeveloper <webmaster@baustein.ca>
2016-09-29 17:01:53 -0400

    commit

--- app/js/custom.js
+++ app/js/custom.js
@@ -55,7 +55,7 @@ $(document).ready(function() {
     $(".input-datepicker").datepicker({
       format: "dd MM, yyyy"
     });
-    $(".input-startdate, .input-enddate, .input-startingdate, .input-endingdate, .input-closingdate").datepicker({
+    $(".input-startdate, .input-enddate, #input-startingdate, #input-endingdate, #input-closingdate").datepicker({
       format: "yyyy-mm-dd"
     });
     $("#fancytree, .all-tasks").fancytree();
